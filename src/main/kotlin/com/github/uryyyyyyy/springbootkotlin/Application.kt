705bfb3cc0a4698a902da1371af96f193a396abf
uryyyyyyy <koki305@gmail.com>
2017-10-09 12:21:42 +0900

    controller

--- src/main/kotlin/com/github/uryyyyyyy/springbootkotlin/Application.kt
+++ src/main/kotlin/com/github/uryyyyyyy/springbootkotlin/Application.kt
@@ -3,9 +3,8 @@ package com.github.uryyyyyyy.springbootkotlin
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
-
 @SpringBootApplication
-open class Application {
+class Application {
   companion object {
     @JvmStatic fun main(args: Array<String>) {
       SpringApplication.run(Application::class.java, *args)
--- src/main/kotlin/com/github/uryyyyyyy/springbootkotlin/controller/HelloController.kt
+++ src/main/kotlin/com/github/uryyyyyyy/springbootkotlin/controller/HelloController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/hello")
-open class HelloController {
+class HelloController {
 
   @GetMapping("/")
   fun hello(): String {
