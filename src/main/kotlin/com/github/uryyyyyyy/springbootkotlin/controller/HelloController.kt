a383af0713d8e1451eb2f17213b460cfa972a045
uryyyyyyy <koki305@gmail.com>
2017-10-09 11:32:00 +0900

    logging

--- src/main/kotlin/com/github/uryyyyyyy/springbootkotlin/controller/HelloController.kt
+++ src/main/kotlin/com/github/uryyyyyyy/springbootkotlin/controller/HelloController.kt
@@ -1,6 +1,7 @@
 package com.github.uryyyyyyy.springbootkotlin.controller
 
 import com.github.uryyyyyyy.springbootkotlin.service.HelloService
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -11,13 +12,17 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/hello")
 class HelloController @Autowired constructor(private val helloService: HelloService) {
 
+  val logger = LoggerFactory.getLogger(HelloController::class.java)!!
+
   @GetMapping("/")
   fun hello(): String {
+    logger.info("hello log")
     return "Hello, World!"
   }
 
   @GetMapping("/{num}")
   fun welcome(@PathVariable("num") number: Int): String {
+    logger.info("welcome {} log", number)
     return helloService.say(number.toString())
   }
 
