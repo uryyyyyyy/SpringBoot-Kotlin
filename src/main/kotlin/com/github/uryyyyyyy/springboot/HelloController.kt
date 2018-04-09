cef2e0a2944608d80ecab6d0b2c4ec007d40cd9d
uryyyyyyy <koki305@gmail.com>
2018-04-10 01:38:46 +0900

    kotlin

---
+++ src/main/kotlin/com/github/uryyyyyyy/springboot/HelloController.kt
@@ -0,0 +1,14 @@
+package com.github.uryyyyyyy.springboot
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+  @GetMapping("/")
+  fun welcome(): String {
+    return "Hello, World!"
+  }
+
+}
--- src/test/kotlin/com/github/uryyyyyyy/springbootkotlin/HelloControllerTest.kt
+++ src/test/kotlin/com/github/uryyyyyyy/springbootkotlin/HelloControllerTest.kt
@@ -1,20 +1,20 @@
 package com.github.uryyyyyyy.springbootkotlin
 
+import com.github.uryyyyyyy.springboot.HelloController
+import org.hamcrest.Matchers.containsString
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
-
-import org.hamcrest.Matchers.containsString
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @RunWith(SpringRunner::class)
-@WebMvcTest(WelcomeController::class)
-class WelcomeControllerTest {
+@WebMvcTest(HelloController::class)
+class HelloControllerTest {
 
     @Autowired
     private val mvc: MockMvc? = null
